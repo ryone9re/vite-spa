204d067c20941158f7bfb624ced49bbfdc9f5bad
ryone9re <ryoichi.kurimoto@gmail.com>
2024-10-29 13:33:51 +0000

    Fix3

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,8 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
@@ -6,8 +10,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const hashHistory = createHashHistory();
+
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
