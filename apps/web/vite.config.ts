1ea6fe12dd74bef16301ccb4dc32691077fd6bf1
ryone9re <ryoichi.kurimoto@gmail.com>
2024-10-29 13:22:53 +0000

    Fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/<REPO>/",
+  base: "/vite-spa/",
   server: {
     host: "127.0.0.1",
   },
